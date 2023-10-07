64cf29d05f0aba4a8d945acc94d54d073fa37e54
asiapicasso <asia.picasso@gmail.com>
2023-10-07 17:33:53 +0200

    Connect to a MongoDB database with Mongoose

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ import logger from "morgan";
 // 1 router per endpoint
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.DATABASE_URL || 'mongodb://localhost/express-api');
 
 const app = express();
 
