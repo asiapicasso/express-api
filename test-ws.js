8df8b865fdf715b449778cf9a43e78c9aef49a47
asiapicasso <asia.picasso@gmail.com>
2023-11-25 18:16:59 +0100

    verify endpoint

--- test-ws.js
+++ test-ws.js
@@ -1,7 +1,6 @@
 import http from 'http';
 import { createWebSocketServer, broadcastMessage } from './ws.js';
 
-
 // Create an HTTP server
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
